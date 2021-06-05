2da0fcf74bdd13b1f492b9cbd8c792ff502912be
Artem Golovinsky <artemgolovinsky@gmail.com>
2021-06-05 23:16:17 +0300

    Update timers

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use panic_halt as _;
 
 use stm32f4::stm32f429;
 use cortex_m_rt::entry;
+use cortex_m;
 
 use clocking::*;
 //use timers;
@@ -28,28 +29,32 @@ fn main() -> ! {
     let mut periph = stm32f429::Peripherals::take().unwrap();
     let cl = Clocking::new(&mut periph.RCC);
     cl.init(&mut periph.FLASH);
-    timers::init(periph.TIM6);
-
-    
+    cl.gpiog_enable();
     let gpiog = &periph.GPIOG;
     gpiog.moder.modify(|_, w| w.moder13().output());
     gpiog.otyper.modify(|_, w| w.ot13().push_pull());
     gpiog.ospeedr.modify(|_, w| w.ospeedr13().very_high_speed());
-    gpiog.odr.modify(|_, w| w.odr13().high());
-
-
-    let mut t1 = timers::new_once(2000, || {
-        gpiog.odr.modify(|_, w| w.odr13().low());
-    });
-
-    let mut t2 = timers::new_once(2000, || {
-        gpiog.odr.modify(|_, w| w.odr13().high());
-    });
-
+   // gpiog.odr.modify(|_, w| w.odr13().high());
+    cl.tim6_enable();
+    timers::init(periph.TIM6);
+    let mut repeat = timers::new_repeat(1000, |state, handler| {
+        if *state == 6 {
+            handler.update_ms(250);
+        }
+        if *state == 12 {
+            handler.stop();
+        }
+        *state+=1;
+        gpiog.odr.modify(|r, w| {
+            if r.odr13().is_low() {
+                w.odr13().high()
+            } else {
+                w.odr13().low()
+            }
+        });
+    }, 1).unwrap();
     loop {
-        t1.take().unwrap().main_loop();
-        t2.take().unwrap().main_loop();
-
+        repeat.main_loop();
         continue;
     }
 }
--- src/timers.rs
+++ src/timers.rs
@@ -3,6 +3,7 @@ use stm32f4::stm32f429;
 use stm32f429::interrupt;
 use core::ops::Fn;
 use core::sync::atomic::{AtomicI32, Ordering};
+use core::cell::RefCell;
 
 
 pub static T1: AtomicI32 = AtomicI32::new(-1);
@@ -11,11 +12,18 @@ pub static T3: AtomicI32 = AtomicI32::new(-1);
 pub static T4: AtomicI32 = AtomicI32::new(-1);
 
 
+static mut TIM6: RefCell<Option<stm32f429::TIM6>> = RefCell::new(None);
+
 pub fn init(tim: stm32f429::TIM6) {
-    tim.psc.write(|w| w.psc().bits(90-1));
-    tim.arr.write(|w| w.arr().bits(1000-1));
-    tim.dier.modify(|_, w| w.uie().enabled());
-    tim.cr1.modify(|_, w| w.cen().enabled());
+    unsafe { 
+        tim.psc.write(|w| w.psc().bits(90-1));
+        tim.arr.write(|w| w.arr().bits(1000-1));
+        tim.dier.modify(|_, w| w.uie().enabled());
+        tim.cr1.modify(|_, w| w.cen().enabled());
+        stm32f429::NVIC::unmask(stm32f429::interrupt::TIM6_DAC);
+        TIM6.replace(Some(tim));
+    };
+
 }
 
 
@@ -43,7 +51,7 @@ pub struct Once<T: Fn() -> ()> {
 
 impl <T: Fn() -> ()>Once<T> {
     pub fn main_loop(&self) {
-        if let 0 = self.at.load(Ordering::Relaxed) {
+        if self.at.load(Ordering::Relaxed) == 0 {
             self.at.store(-1, Ordering::Relaxed);
             (self.clb)();
         }
@@ -63,8 +71,87 @@ pub fn new_once<T>(ms: u16, clb: T) -> Option<Once<T>>
 }
 
 
+pub struct Repeat<T: Fn(&mut S, &mut RepeatHandler) -> (), S> {
+    clb: T,
+    at: Option<&'static AtomicI32>,
+    ms: u16,
+    state: S,
+    stopped: bool
+}
+
+
+impl <T: Fn(&mut S, &mut RepeatHandler) -> (), S> Repeat<T, S> {
+    pub fn main_loop(&mut self) {
+        if let Some(at) = self.at {
+            if at.load(Ordering::Relaxed) == 0 {
+                let mut handler = RepeatHandler{
+                    ms: 0, 
+                    ms_updated: false, 
+                    stopped: false
+                };
+                (self.clb)(&mut self.state, &mut handler);
+                if handler.stopped {
+                    self.stop();
+                    return
+                }
+                if handler.ms_updated {
+                    self.ms = handler.ms;
+                }
+                at.store(i32::from(self.ms), Ordering::Relaxed);
+            }
+        }
+    }
+    pub fn stop(&mut self) {
+        if let Some(at) = self.at {
+            at.store(-1, Ordering::Relaxed);
+            self.at = None
+        }
+    }
+}
+
+
+
+
+
+pub fn new_repeat<T, S>(ms: u16, clb: T, state: S) -> Option<Repeat<T, S>> 
+    where T: Fn(&mut S, &mut RepeatHandler) -> () {
+    if let Some(at) = acquire() {
+        at.store(i32::from(ms), Ordering::Relaxed);
+        return Some(Repeat{
+            clb: clb,
+            at: Some(at),
+            ms: ms,
+            state: state,
+            stopped: false
+        }) 
+    }
+    None
+}
+
+
+pub struct RepeatHandler {
+    ms: u16,
+    stopped: bool,
+    ms_updated: bool
+}
+
+impl RepeatHandler {
+    pub fn update_ms(&mut self, ms: u16) {
+        self.ms = ms;
+        self.ms_updated = true
+    }
+    pub fn stop(&mut self) {
+        self.stopped = true;
+    }
+}
+
 #[interrupt]
 fn TIM6_DAC() {
+    unsafe {
+        let tim6 = TIM6.borrow();
+        tim6.as_ref().unwrap().sr.modify(|_, w| w.uif().clear_bit());
+    }
+    
     let f = |t| {
         if t > 0 { Some(t - 1) }
         else { None }
@@ -73,5 +160,6 @@ fn TIM6_DAC() {
     T2.fetch_update(Ordering::Relaxed, Ordering::Relaxed, f);
     T3.fetch_update(Ordering::Relaxed, Ordering::Relaxed, f);
     T4.fetch_update(Ordering::Relaxed, Ordering::Relaxed, f);
+    
     ()
 }
